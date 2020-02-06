41411a5b6599459758564c7a8d022359fba7be3d
juanzitelli <juanzitelli7@gmail.com>
2020-02-06 08:01:17 -0300

    New port: 7000, gonna deploy on CONTABO server

--- api.py
+++ api.py
@@ -1022,5 +1022,5 @@ def etiquetado_morfologico(codigo):
 # endregion
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=7000)
     # app.run(debug=True)
